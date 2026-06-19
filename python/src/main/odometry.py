c03b6b86e0cb88db6ae032a86828603bfc526adb
Charles Kennedy <82charkenn@csdecatur.net>
2026-06-18 20:04:35 -0400

    odometry changes pi5

--- python/src/main/odometry.py
+++ python/src/main/odometry.py
@@ -1,20 +1,19 @@
-import mpu6050 
+from mpu6050 import mpu6050
 from time import sleep
-
-accelerometer = mpu6050(0x68)
 
 velocity = 0
 position = 0
+accel = mpu6050.mpu6050(0x68)
 
 target_decceleration = 0
 
 target_position = 10
 
 def get_acceleration():
-    return accelerometer.get_accel_data()
+    return accel.get_accel_data()
 
 def get_gyro():
-    return accelerometer.get_gyro_data()
+    return accel.get_gyro_data()
 
 def get_error():
     return target_position - position
@@ -32,5 +31,5 @@ while True:
     accel_data = get_acceleration()
     velocity += accel_data['x'] * 0.01
     position += velocity * 0.01 
-    print(accelerometer.get_accel_data())
+    print(accel.get_accel_data())
     sleep(0.01)
